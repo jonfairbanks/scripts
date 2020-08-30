0c19572f958b0370d65624665920ddf6b790139d
Jon Fairbanks <fairplay89@gmail.com>
2020-08-29 20:26:21 -0700

    Set exit message

--- pull-all-docker.sh
+++ pull-all-docker.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
-curl https://registry.hub.docker.com/v2/repositories/$1/ --silent | jq '.results'
+if [ -z "$1" ]
+  then echo "Error: Please specify a Dockerhub user"
+  exit
+fi
+
+echo "Dockerhub User: $1\n\n"
+
+IMAGES=$(curl https://registry.hub.docker.com/v2/repositories/$1/ --silent | jq -r '.results | .[] | .name')
+
+for i in $IMAGES
+do
+  echo "Pulling all tags for image: $1/$i"
+  docker pull $1/$i --all-tags --quiet > /dev/null 2>&1
+  docker images | grep $i | tr -s ' ' | cut -d ' ' -f 2 | xargs -I {} docker rmi $1/$i:{} > /dev/null 2>&1
+  echo "Cleaned up pulled images for: $1/$i\n"
+done
+
+echo "[OK] All Docker images have been pulled for $1"
