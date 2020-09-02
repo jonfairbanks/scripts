3eb95b14f892bbaa60e57c3a5666b8ca489fb5ff
Jon Fairbanks <fairplay89@gmail.com>
2020-09-01 20:48:12 -0700

    Run in Parallel

--- pull-all-docker.sh
+++ pull-all-docker.sh
@@ -1,20 +1,37 @@
 #!/bin/sh
 
+PIDS=""
+RESULT=0
+USER=""
+IMAGES=""
+
 if [ -z "$1" ]
-  then echo "Error: Please specify a Dockerhub user"
-  exit
+  then 
+    echo "Error: Please specify a Docker Hub user"
+    exit 1
+else
+  USER=$1
+  IMAGES=$(curl https://registry.hub.docker.com/v2/repositories/$1/ --silent | jq -r '.results | .[] | .name')
+  echo "Pulling images/tags for Docker Hub user: $USER\n"
 fi
 
-echo "Dockerhub User: $1\n\n"
-
-IMAGES=$(curl https://registry.hub.docker.com/v2/repositories/$1/ --silent | jq -r '.results | .[] | .name')
+pull_and_clean() {
+  docker pull $USER/$i --all-tags --quiet > /dev/null 2>&1
+  docker images | grep $i | tr -s ' ' | cut -d ' ' -f 2 | xargs -I {} docker rmi $USER/$i:{} > /dev/null 2>&1
+  echo "Pulled all tags for image: $USER/$i"
+}
 
 for i in $IMAGES
 do
-  echo "Pulling all tags for image: $1/$i"
-  docker pull $1/$i --all-tags --quiet > /dev/null 2>&1
-  docker images | grep $i | tr -s ' ' | cut -d ' ' -f 2 | xargs -I {} docker rmi $1/$i:{} > /dev/null 2>&1
-  echo "Cleaned up pulled images for: $1/$i\n"
+  pull_and_clean $i &
+  PIDS="$PIDS $!"
 done
 
-echo "[OK] All Docker images have been pulled for $1\n"
+for pid in $PIDS; do
+  wait $pid || let "RESULT=1"
+done
+
+if [ "$RESULT" -eq 1 ];
+  then
+    exit 1
+fi
