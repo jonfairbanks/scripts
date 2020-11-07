02c0ea6c493c60d4b4e1984fb6b64c426c7fbb5b
Jon Fairbanks <fairplay89@gmail.com>
2020-11-06 17:12:12 -0800

    Create sys-info.py

---
+++ sys-info.py
@@ -0,0 +1,69 @@
+import argparse
+import os
+import paramiko
+import sys
+
+__version__ = "1.0.0"
+
+parser = argparse.ArgumentParser(description='Get SysInfo for Remote Systems')
+parser.add_argument("-f", "--file", help="Input file for bulk IPs", type=str)
+parser.add_argument("-u", "--username", help="SSH username to use for remote connections", type=str, required=True)
+parser.add_argument("-p", "--password", help="SSH password to use for remote connections", type=str, required=True)
+parser.add_argument("-c", "--cmd", help="Command to run on remote hosts", type=str)
+parser.add_argument("-v", "--version", help="current SysInfo version.", action="store_true")
+args = parser.parse_args()
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+def get_sys_info(ip, cmd):     
+    global client
+    try:
+        print(f"Connecting to " + str(ip).rstrip() + "...")
+        client.connect(ip, username=args.username, password=args.password, timeout=3)
+
+        stdin, stdout, stderr = client.exec_command(cmd)
+
+        for line in stdout:
+            print("Output: " + line)
+
+        client.close()
+    except Exception as e:
+        print("Error: ", e, end="\n\n")
+        pass
+
+def main():
+    """main func."""
+
+    if args.version:
+        print(f"Current Version: {__version__}\n")
+        sys.exit(0)
+
+    if args.file:
+        file = open(args.file, 'r') 
+    else:
+        file = open('hosts.txt', 'r') 
+
+    if args.cmd:
+        cmd = args.cmd
+    else:
+        cmd = "uptime | awk -F'( |,|:)+' '{print $6,$7\",\",$8,\"hours,\",$9,\"minutes.\"}'"
+
+    count = 0
+    while True: 
+        count += 1
+        ip = file.readline()
+        if not ip: 
+            break
+        get_sys_info(ip, cmd)
+    
+    file.close() 
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
